af1c072c353f50a13df2c6b9bee0aa6107d19c50
louvresoft <louvresoft@gmail.com>
2022-09-25 16:19:14 -0500

    Manejo de archivos

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from typing import Optional
 from enum import Enum
 
 # Pydantic
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 
 # FastAPI
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Cookie, UploadFile, File
 from fastapi import Body, Query, Path, Form, status
 
 app = FastAPI()
@@ -112,3 +112,35 @@ def update_person(
 )
 def login(username: str = Form(...), password: str = Form(...)):
     return LoginOut(username=username)
+
+
+@app.post(path="/contact", status_code=status.HTTP_200_OK)
+def contact(
+        first_name: str = Form(
+            ...,
+            max_length=20,
+            min_length=1
+        ),
+        last_name: str = Form(
+            ...,
+            max_length=20,
+            min_length=1
+        ),
+        email: EmailStr = Form(...),
+        message: str = Form(..., min_length=20),
+        user_agent: Optional[str] = Header(default=None),
+        ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
+
+
+# Files
+@app.post(path="/post-image")
+def post_image(
+        image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+    }
