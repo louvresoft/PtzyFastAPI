1b28b057cdffcb9d90059710137ae420a61a6c53
louvresoft <louvresoft@gmail.com>
2022-09-26 12:16:45 -0500

    Se crean modelos

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
+# Python
+from uuid import UUID
+from datetime import date, datetime
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# Fast API
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+# Modelos
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+
+class User(UserBase):
+    nombre: str = Field(..., min_length=1, max_length=50)
+    apellido_paterno: str = Field(..., min_length=1, max_length=50)
+    fecha_nacimiento: Optional[date] = Field(default=None)
+
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(...)
+    content: str = Field(..., min_length=1, max_length=256)
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 
 @app.get(path="/")
